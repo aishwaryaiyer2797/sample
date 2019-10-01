24ce91283c14f55939e7afc00a8f35263a194f80
aishwaryaiyer2797 <aishwarya0697@gmail.com>
2019-10-01 17:03:05 +0530

    linux

--- code/linux/shell15.sh
+++ code/linux/shell15.sh
@@ -11,7 +11,9 @@ per=`expr $c / 3`
 
 var1=`cat a1|grep "$name"|cut -d" " -f1`
 var2=`cat a1|grep "$name"|cut -d" " -f2`
-
+var3=`cat a1|grep "$name"|cut -d" " -f3`
 cat a1|grep -v "$name">a4
 mv a4 a1
-echo "$var1 $var2 $m1 $m2 $m3 $c $per">>a1
+
+echo "$var1 $var2 $var3 $m1 $m2 $m3 $c $per">>a1
+cat a1|sort -n
